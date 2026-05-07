3ac61113fa003891ff9dfa1b54805de96162e9fe
prabhav266 <prabhav.2502@gmail.com>
2026-05-07 23:17:39 +0530

    Created failing CI workflow for testing

--- apps/api/routes/ai.py
+++ apps/api/routes/ai.py
@@ -15,10 +15,17 @@ def summarize_event(event: dict):
     try:
 
         prompt = f"""
-        Summarize this GitHub event in simple English:
+                    You are an AI DevOps assistant.
 
-        {event}
-        """
+                    Analyze this GitHub event and explain:
+                    - what happened
+                    - whether the workflow succeeded or failed
+                    - possible reason for failure
+
+                    GitHub Event:
+
+                    {event}
+                    """
 
         response = model.generate_content(prompt)
 
@@ -39,6 +46,57 @@ def summarize_event(event: dict):
             summary = (
                 f"Webhook connected successfully "
                 f"to {repository_name}"
+            )
+
+        elif event_type == "workflow_run":
+
+            workflow = event.get("workflow", {})
+            conclusion = event.get("conclusion")
+
+            workflow_name = workflow.get(
+                "name",
+                "Unknown Workflow"
+            )
+
+            if conclusion == "success":
+
+                summary = (
+                    f"CI workflow '{workflow_name}' "
+                    f"completed successfully "
+                    f"in {repository_name}"
+                )
+
+            elif conclusion == "failure":
+
+                summary = (
+                    f"CI workflow '{workflow_name}' "
+                    f"failed in {repository_name}. "
+                    f"Possible issue detected in pipeline execution."
+                )
+
+            else:
+
+                summary = (
+                    f"CI workflow '{workflow_name}' "
+                    f"ran in {repository_name}"
+                )
+
+        elif event_type == "workflow_job":
+             summary = (
+                f"A CI/CD job ran inside "
+                f"{repository_name}"
+            )
+
+        elif event_type == "check_run":
+            summary = (
+                f"A validation check was "
+                f"performed in {repository_name}"
+            )
+
+        elif event_type == "check_suite":
+            summary = (
+                f"A CI validation suite "
+                f"executed in {repository_name}"
             )
 
         else:
