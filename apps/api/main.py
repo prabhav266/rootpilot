8da3e16cea724620709debeac44ee364d93fd11d
prabhav266 <prabhav.2502@gmail.com>
2026-05-22 00:34:08 +0530

    deployement changes

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI
-from database import engine,Base
-from models.user import User
-from routes.health import router as health_router
-from models.repository import Repository
-from routes.repositories import router as repo_router
 from fastapi.middleware.cors import CORSMiddleware
+
+from database import engine, Base
+
+from models.user import User
+from models.repository import Repository
 from models.event import Event
+
+from routes.health import router as health_router
+from routes.repositories import router as repo_router
 from routes.webhooks import router as webhook_router
 from routes.ai import router as ai_router
 from routes.ci_debug import router as ci_debug_router
 from routes.websocket_route import (
     router as websocket_router
 )
+from routes.events import router as events_router
 
+# Create DB tables
 Base.metadata.create_all(bind=engine)
 
+# FastAPI app
+app = FastAPI()
 
-app = FastAPI() #fun
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -28,17 +35,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Routers
 app.include_router(health_router)
+
 app.include_router(repo_router)
+
 app.include_router(
     webhook_router,
     prefix="/webhooks/github"
 )
+
+app.include_router(events_router)
+
 app.include_router(ai_router)
+
 app.include_router(ci_debug_router)
 
+# Optional websocket router
+# app.include_router(websocket_router)
 
-
+# Root endpoint
 @app.get("/")
 def root():
     return {"message": "RootPilot API Running"}
